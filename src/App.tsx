c8ac4b2b2ba93a91e29275b564018a108bc66c0a
Dev-DanyVazquez69 <danyv7818@gmail.com>
2024-06-22 12:45:54 -0300

    resolvido problema em build

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,10 @@ import Footer from './components/shared/footer/footer';
 //<outlet/> se refere ao conteudo dinamico de cada page
 
 function App() {
-
 	return (
 		<ContainerIndex>
 			<Header />
-			<Outlet>
-			</Outlet>
+			<Outlet />
 			<Footer />
 		</ContainerIndex>
 	);
